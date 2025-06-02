eb8da2c7865231a5d511aa68c33cb44b4717d71f
Lars Gielen <larsgielen2003@gmail.com>
2025-06-02 19:15:13 +0200

    Update Dotfiles

--- install_scripts/dotfiles_install.sh
+++ install_scripts/dotfiles_install.sh
@@ -35,29 +35,34 @@ sudo pacman -S --needed --noconfirm ttf-fira-sans ttf-font-awesome ttf-roboto tt
 # install basic control applications
 sudo pacman -S --needed --noconfirm pipewire wireplumber hyprpolkitagent xdg-desktop-portal-hyprland qt5-wayland qt6-wayland xorg-xhost
 
-###########################
-### Thunar File Browser ###
-###########################
-
-# install thunar and plugins
-sudo pacman -S --needed --noconfirm thunar thunar-media-tags-plugin tumbler
-
-# thunar auto mount support
-sudo pacman -S --needed --noconfirm thunar-volman gvfs gvfs-mtp gvfs-smb gvfs-gphoto2 udisks2
-
-# install xarchiver with thunar support (extract here functionality)
-sudo pacman -S --needed --noconfirm thunar-archive-plugin xarchiver p7zip tar unrar unzip
-
 ############################
 ### General Applications ###
 ############################
-sudo pacman -S --needed --noconfirm kitty pavucontrol blueman fastfetch htop
-yay -S --needed --noconfirm zen-browser-bin visual-studio-code-bin checkupdates-with-aur 
+
+# install thunar and plugins
+sudo pacman -S --needed --noconfirm thunar thunar-media-tags-plugin tumbler
+# thunar auto mount support
+sudo pacman -S --needed --noconfirm thunar-volman gvfs gvfs-mtp gvfs-smb gvfs-gphoto2 udisks2
+# xarchiver with thunar support (extract here functionality)
+sudo pacman -S --needed --noconfirm thunar-archive-plugin xarchiver p7zip tar unrar unzip
+
+# printing support
+sudo pacman -S --needed --noconfirm cups cups-pdf ghostscript gutenprint foomatic-db-engine
+
+# other applications
+sudo pacman -S --needed --noconfirm kitty pavucontrol blueman fastfetch htop vivaldi
+yay -S --needed --noconfirm visual-studio-code-bin checkupdates-with-aur 
+
+# Flatpak
+sudo pacman -S --needed --noconfirm flatpak
+flatpak install -y flathub com.github.tchx84.Flatseal 
+flatpak install -y flathub md.obsidian.Obsidian 
 
 #################################
 ### Post-installation cleanup ###
 #################################
 systemctl --user enable --now hyprpolkitagent.service
-sudo systemctl enable bluetooth 
+sudo systemctl enable --now cups.service
+sudo systemctl enable --now bluetooth.service
 
 echo "Everything is installed, DONT FORGET TO ENABLE SYSTEM SERVICES AND FIREWALL, I CANT DO EVERYTHING FOR YOU!!"
