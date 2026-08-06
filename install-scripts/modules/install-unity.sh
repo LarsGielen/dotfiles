3a1ac7f5cbc5d98a5970c8be9da6388d184b4190
Lars Gielen <larsgielen2003@gmail.com>
2026-08-06 13:27:58 +0200

    Update Dotfiles

--- install-scripts/modules/install-unity.sh
+++ install-scripts/modules/install-unity.sh
@@ -10,3 +10,8 @@ install_packages \
     aspnet-runtime
 
 install_aur unityhub
+
+# install unity cli
+run_quiet curl -fsSL https://public-cdn.cloud.unity3d.com/hub/prod/cli/install.sh | UNITY_CLI_CHANNEL=beta bash
+
+# Todo: In future we could install a unity version right away, and unityhub can be removed if I do all hub work via the terminial, which would be cool.
